3b0a9ac3dc64cf5d1a2f747b122723f4f68de75a
Judge Muzinda <jmuzinda@gmail.com>
2019-08-07 15:52:45 +0200

    Updated base classes

--- android/app/src/main/java/com/example/ehr_mobile/model/BasePageable.java
+++ android/app/src/main/java/com/example/ehr_mobile/model/BasePageable.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import lombok.Data;
 
-@Data
+//@Data
 public class BasePageable <T> implements Serializable {
 
     private int totalElements;
@@ -17,4 +17,91 @@ public class BasePageable <T> implements Serializable {
     private  int size;
     private int number;
     private List<T> content;
+
+    public int getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(int totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isSort() {
+        return sort;
+    }
+
+    public void setSort(boolean sort) {
+        this.sort = sort;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public void setNumberOfElements(int numberOfElements) {
+        this.numberOfElements = numberOfElements;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "BasePageable{" +
+                "totalElements=" + totalElements +
+                ", last=" + last +
+                ", totalPages=" + totalPages +
+                ", sort=" + sort +
+                ", first=" + first +
+                ", numberOfElements=" + numberOfElements +
+                ", size=" + size +
+                ", number=" + number +
+                ", content=" + content +
+                '}';
+    }
 }
--- android/app/src/main/java/com/example/ehr_mobile/model/Occupation.java
+++ android/app/src/main/java/com/example/ehr_mobile/model/Occupation.java
@@ -2,9 +2,33 @@ package com.example.ehr_mobile.model;
 
 import lombok.Data;
 
-@Data
+//@Data
 public class Occupation extends BasePageable<Occupation> {
 
     private String code;
     private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ " Occupation{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
