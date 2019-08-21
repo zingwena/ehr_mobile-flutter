15ac7e0a9dd6b871077447dc6c7d6755f778ce3e
kchikweshe <kchikweshe@hitrac.co.zw>
2019-08-21 16:52:51 +0200

    add function to pull testkits

---
+++ android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/persistance/dao/TestKitDao.java
@@ -0,0 +1,31 @@
+package zw.gov.mohcc.mrs.ehr_mobile.persistance.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import zw.gov.mohcc.mrs.ehr_mobile.model.TestKit;
+
+/**
+ * @author kombo on 8/21/19
+ */
+@Dao
+public interface TestKitDao {
+    @Insert
+    void insertTestKits(List<TestKit> testKits);
+
+    @Query("DELETE FROM TestKit")
+    void deleteTestKits();
+
+
+    @Insert
+    void insertTestKit(TestKit testKit);
+
+    @Query("SELECT * FROM TestKit ORDER BY  name ASC")
+    List<TestKit> getAllTestKits();
+
+    @Query("SELECT * FROM TestKit WHERE id=:id")
+    TestKit findTestKitById(int id);
+}
