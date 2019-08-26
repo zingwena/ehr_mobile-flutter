4f5abb8116763d693328ad9b33433c6c576f7c6a
Nokuthaba <lunganokuthaba@gmail.com>
2019-08-26 15:16:21 +0200

    hts screens

---
+++ android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/model/Sample.java
@@ -0,0 +1,11 @@
+package zw.gov.mohcc.mrs.ehr_mobile.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Sample extends BaseNameModel{
+    @PrimaryKey
+    private int Id;
+
+}
