27982e2decdba78e2bdde6a7aac2fed0523fa233
Judge Muzinda <jmuzinda@gmail.com>
2019-11-06 15:23:51 +0200

    Added read side of hts screening

--- android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/persistance/dao/HtsDao.java
+++ android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/persistance/dao/HtsDao.java
@@ -49,7 +49,7 @@ public interface HtsDao {
     @Query("SELECT count(*) FROM Hts WHERE personId =:personId")
     int  countByPersonId(String personId);
 
-    @Query("SELECT h.* FROM Hts h inner join Visit v on v.personId = h.personId WHERE h.personId=:personId Order By v.visitStartDate Desc limit 0,1")
+    @Query("SELECT h.* FROM Hts h inner join Visit v on v.personId = h.personId WHERE h.personId=:personId Order By v.visitStartDate Desc limit 1,0")
     Hts findLatestHts(String personId);
 
     @Query("SELECT * FROM Hts Where laboratoryInvestigationId=:laboratoryInvestigationId")
