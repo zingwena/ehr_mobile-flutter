b244c4196a4e0fd240130dfcdfc71b814bf80d88
thomas <tomchanyowedza@gmail.com>
2019-11-21 13:26:35 +0200

    Title Standardization

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/amushate/Documents/ICAP-Projects/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/amushate/Documents/ICAP-Projects/ehr_mobile"
+export "FLUTTER_ROOT=/home/thomas/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/thomas/Desktop/ehr_mobile"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/amushate/Documents/ICAP-Projects/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/thomas/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
