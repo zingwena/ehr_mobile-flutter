bec102eb20b8c49bd22cf562c273d30669f4b9cc
Judge Muzinda <jmuzinda@gmail.com>
2020-01-15 10:24:51 +0200

    Added person age calculation to personService

--- android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/service/PersonService.java
+++ android/app/src/main/java/zw/gov/mohcc/mrs/ehr_mobile/service/PersonService.java
@@ -1,6 +1,8 @@
 package zw.gov.mohcc.mrs.ehr_mobile.service;
 
 
+import zw.gov.mohcc.mrs.ehr_mobile.dto.Age;
+import zw.gov.mohcc.mrs.ehr_mobile.model.person.Person;
 import zw.gov.mohcc.mrs.ehr_mobile.persistance.database.EhrMobileDatabase;
 
 public class PersonService {
@@ -10,5 +12,11 @@ public class PersonService {
 
     public PersonService(EhrMobileDatabase ehrMobileDatabase) {
         this.ehrMobileDatabase = ehrMobileDatabase;
+    }
+
+    public Age getAge(String personId) {
+
+        Person person = ehrMobileDatabase.personDao().findPatientById(personId);
+        return Age.getInstance(person);
     }
 }
