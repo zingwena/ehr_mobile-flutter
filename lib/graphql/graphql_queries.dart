c80e23d3b2800dcdff746d474f51525dce6ad8ba
Anesu Mushate <anesu.mushate@econet.co.zw>
2020-02-04 14:06:44 +0200

    query update

--- lib/graphql/graphql_queries.dart
+++ lib/graphql/graphql_queries.dart
@@ -6,36 +6,131 @@
       return """ 
         query GetPatientsQuery{
   
-          people(text: "", page: 0, sort: "", size: 15) {
-                  content {
-                      personId
-                      firstname
-                      lastname
-                      phones{number,phoneId}
-                      sex
-                      identifications{number,type{name}}
-                      occupation{id,name}
-                      identifications{identificationId}
-                      selfIdentifiedGender
-                      religion{name,id}
-                      education{id,name}
-                      marital{name,id}
-                      birthdate
-                      nationality{id,name}
-                      countryOfBirth{id,name}
-                      address {
-                          city
-                          town {
-                              name
-                          }
-                          street
-                      }
-                      age {
-                          years
-                      }
-                  }
-              }
-          }
+            people(text: "", page: 0, size: 3, sort: "") {
+                content {
+                personId
+                firstname
+                lastname
+                phones {
+                    number
+                    phoneId
+                }
+                sex
+                identifications {
+                    number
+                    type {
+                    name
+                    }
+                }
+                occupation {
+                    id
+                    name
+                }
+                identifications {
+                    identificationId
+                }
+                selfIdentifiedGender
+                religion {
+                    name
+                    id
+                }
+                education {
+                    id
+                    name
+                }
+                marital {
+                    name
+                    id
+                }
+                birthdate
+                nationality {
+                    id
+                    name
+                }
+                countryOfBirth {
+                    id
+                    name
+                }
+                address {
+                    city
+                    town {
+                    name
+                    }
+                    street
+                }
+                age {
+                    years
+                }
+                hivStatus {
+                    date
+                    status
+                }
+                visitHistory {
+                    patientId
+                    time
+                    type
+                    hospitalNumber
+                    discharged
+                    registeredOnArt
+                    cbsNotificationDone
+                    screenTbPatient
+                    facility{
+                    id
+                    name
+                    }
+                    hts {
+                    htsId
+                    date
+                    coupleCounselling
+                    approach
+                    retest
+                    preTestInformationGiven
+                    resultsIssued
+                    testForPregnancy
+                    postTestCounselling
+                    htsNumber
+                    optOut
+                    postDateCounselled
+                    dateOfHivTest
+                    pregnant
+                    lactating
+                    consentToIndexTesting
+                    htsType
+                    selfTestPositive
+                    startedLactating
+                    reasonForNotInitiatingArt
+                    reasonForNotPerformingTest
+                    firstHivTest
+                    performDnaPcr
+                    dnaPcrInvestigationOrderId
+                    dnaPcrDone
+                    recencyInvestigationOrderId
+                    clientAlreadyPositive
+                    clientAlreadyOnArt
+                    laboratoryInvestigation {
+                        laboratoryInvestigationId
+                        date
+                        date
+                        investigationId
+                        resultDate
+                        status
+                        investigationType
+                        openHivTest
+                    }
+                    entryPoint {
+                        id
+                    }
+                    model {
+                        id
+                    }
+                    reasonForNotIssuingResult {
+                        id
+                    }
+                    }
+                }
+                }
+            }
+
       """;
     }
 
