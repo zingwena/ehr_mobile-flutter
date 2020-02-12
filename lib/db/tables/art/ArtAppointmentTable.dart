553a56ddc36ef65a825b3c885f4872eba84031ff
Anesu Mushate <anesumushate@gmail.com>
2020-02-12 15:35:11 +0200

    fetch art appointments from EHR

---
+++ lib/db/tables/art/ArtAppointmentTable.dart
@@ -0,0 +1,67 @@
+
+
+import 'package:ehr_mobile/util/custom_date_converter.dart';
+
+import '../base_table.dart';
+
+class ArtAppointmentTable extends BaseTable{
+
+    String artId;
+
+    String reason_code;
+    String reason_name;
+
+    String date;
+
+    String followUpReason_code;
+    String followUpReason_name;
+
+    String followupDate;
+    String appointmentOutcomeDate;
+
+    String appointmentOutcome_code;
+    String appointmentOutcome_name;
+
+    static ArtAppointmentTable fromJson(Map map) {
+      var art = ArtAppointmentTable();
+
+      art.id=map['id'];
+      art.status=map['status'];
+
+      art.artId = map['artId'];
+
+      art.reason_code = map['reason_code'];
+      art.reason_name = map['reason_name'];
+
+      art.date = const CustomDateTimeConverter().fromIntToSqlDate(map['date']);
+
+      art.followUpReason_code = map['followUpReason_code'];
+      art.followUpReason_name = map['followUpReason_name'];
+
+      art.followupDate = const CustomDateTimeConverter().fromIntToSqlDate(map['followupDate']);
+      art.appointmentOutcomeDate = const CustomDateTimeConverter().fromIntToSqlDate(map['appointmentOutcomeDate']);
+
+      art.appointmentOutcome_code=map['appointmentOutcome_code'];
+      art.appointmentOutcome_name=map['appointmentOutcome_name'];
+
+      return art;
+    }
+
+    Map<String, dynamic> toJson() => {
+      'id':id,
+      'status':status,
+      'artId':artId,
+      'reason_code':reason_code,
+      'reason_name':reason_name,
+
+      'date':date,
+      'followUpReason_code':followUpReason_code,
+      'followUpReason_name':followUpReason_name,
+
+      'followupDate':followupDate,
+      'appointmentOutcomeDate':appointmentOutcomeDate,
+
+      'appointmentOutcome_code':appointmentOutcome_code,
+      'appointmentOutcome_name':appointmentOutcome_name,
+    };
+}
--- lib/sync/fetch_patient_record.dart
+++ lib/sync/fetch_patient_record.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:ehr_mobile/db/dao/art_dao/ArtAppointmentDao.dart';
 import 'package:ehr_mobile/db/dao/art_dao/art_dao.dart';
 import 'package:ehr_mobile/db/dao/art_dao/ArtSymptomDao.dart';
 import 'package:ehr_mobile/db/dao/blood_pressure_dao.dart';
@@ -37,148 +36,171 @@ Future<String> pullPatientData(ProgressDialog progressDialog) async {
     var personDao = PersonDao(adapter);
     var visitDao = VisitDao(adapter);
     var htsDao = HtsDao(adapter);
-    var personInvestigationDao= PersonInvestigationDao(adapter);
-    var labInvestigationDao=LaboratoryInvestigationDao(adapter);
+    var personInvestigationDao = PersonInvestigationDao(adapter);
+    var labInvestigationDao = LaboratoryInvestigationDao(adapter);
+    await ArtDao(adapter).removeAll();
+    await ArtSymptomDao(adapter).removeAll();
 
     ///Empty existing data first
-    htsDao.removeAll();
-    personInvestigationDao.removeAll();
-    personDao.removeAll();
-    visitDao.removeAll();
-    labInvestigationDao.removeAll();
+    await htsDao.removeAll();
+    await personInvestigationDao.removeAll();
+    await personDao.removeAll();
+    await visitDao.removeAll();
+    await labInvestigationDao.removeAll();
 
     var t = await result.data["people"]['content'];
     for (Map patient in t) {
-      try{
-          log.i(patient);
-          progressDialog.update(message: '${patient['firstname']}  ${patient['lastname']}.....');
-          var personId=patient['personId'];
-          personDao.insertFromEhr(patient);
-          if(patient['history']!=null){
-            var history=patient['history'];
-            await savePersonInvestigations(history,personInvestigationDao,personId);
+      //log.i('--------${patient['firstname']}  ${patient['lastname']}.....');
+      //try{
+      progressDialog.update(
+          message: '${patient['firstname']}  ${patient['lastname']}.....');
+      var personId = patient['personId'];
+      personDao.insertFromEhr(patient);
 
-            //log.i('${patient['firstname']}  ${patient['lastname']}..... $history');
-            ///-------Save Sexual History--------------
-            if(history['sexualHistory']!=null){
-              var sexualHistory = history['sexualHistory'];
-              await saveSexualHistory(sexualHistory);
+        if (patient['history'] != null) {
+          var history = patient['history'];
+          await savePersonInvestigations(
+              history, personInvestigationDao, personId);
 
-              ///-------Save Sexual History Questions--------------
-              if(sexualHistory['questions']!=null){
-                await saveSexualHistoryQuestion(sexualHistory,sexualHistory['sexualHistoryId']);
-              }
-            }
+          ///-------Save Sexual History--------------
+          if (history['sexualHistory'] != null) {
+            var sexualHistory = history['sexualHistory'];
+            await saveSexualHistory(sexualHistory);
 
-          }
-
-          for(Map visitHistory in patient['visitHistory']){
-            var patientId= visitHistory['patientId'];
-            await visitDao.insertFromEhr(visitHistory,patient['personId']);
-            await saveVitals(visitHistory,personId, patientId);
-            if(visitHistory['hts']!=null){
-              if(visitHistory['hts']['laboratoryInvestigation']!=null ){
-                await labInvestigationDao.insertFromEhr(visitHistory['hts']['laboratoryInvestigation'],
-                    visitHistory['facility']['id']);
-              }
-              await htsDao.insertFromEhr(visitHistory['hts'],personId,patientId);
+            ///-------Save Sexual History Questions--------------
+            if (sexualHistory['questions'] != null) {
+              await saveSexualHistoryQuestion(
+                  sexualHistory, sexualHistory['sexualHistoryId']);
             }
           }
-
-          ///-------Save Art --------------
-          if(patient['art']!=null){
-            var art=patient['art'];
-            log.i('${patient['firstname']}  ${patient['lastname']}..... $art');
-            await savePatientArt(art,personId);
-
-            if(art['symptoms']!=null){
-              await savePatientArtSymptoms(art,art['artId']);
-            }
-          }
-
-        }catch(e){
-          log.e('${patient['firstname']}  ${patient['lastname']}..... $e');
-          //throw e;
         }
+
+      for (Map visitHistory in patient['visitHistory']) {
+        var patientId = visitHistory['patientId'];
+        await visitDao.insertFromEhr(visitHistory, patient['personId']);
+
+        await saveVitals(visitHistory, personId, patientId);
+          if (visitHistory['hts'] != null) {
+            if (visitHistory['hts']['laboratoryInvestigation'] != null) {
+              await labInvestigationDao.insertFromEhr(
+                  visitHistory['hts']['laboratoryInvestigation'],
+                  visitHistory['facility']['id']);
+            }
+            await htsDao.insertFromEhr(
+                visitHistory['hts'], personId, patientId,visitHistory['hts']['laboratoryInvestigation']['laboratoryInvestigationId']);
+          }
+
+
+      }
+
+      ///-------Save Art --------------
+      if (patient['art'] != null) {
+        var art = patient['art'];
+        log.i(art);
+        await savePatientArt(art, personId);
+
+        if (art['symptoms'] != null) {
+          await savePatientArtSymptoms(art, art['artId']);
+        }
+
+        if (art['appointments'] != null) {
+          await saveArtAppointments(art);
+        }
+      }
+
+//        }catch(e){
+//          log.e('${patient['firstname']}  ${patient['lastname']}..... $e');
+//          throw e;
+//        }
       //await Future.delayed(Duration(milliseconds: 500));
     }
-  } else{
+  } else {
     log.e('$result');
   }
   return "$DONE_STATUS";
 }
 
-Future<String> savePersonInvestigations(Map map,PersonInvestigationDao dao, String personId) async{
-  if(map['investigations']!=null){
-    for(Map investigation in map['investigations']){
-      await dao.insertFromEhr(investigation,personId);
+Future<String> savePersonInvestigations(
+    Map map, PersonInvestigationDao dao, String personId) async {
+  if (map['investigations'] != null) {
+    for (Map investigation in map['investigations']) {
+      await dao.insertFromEhr(investigation, personId);
     }
   }
   return 'DONE_STATUS';
 }
 
-Future<String> saveVitals(Map visit, String personId,String patientId) async{
+Future<String> saveVitals(Map visit, String personId, String patientId) async {
   var dbHandler = DatabaseHelper();
   var adapter = await dbHandler.getAdapter();
   var tempDao = TemperatureDao(adapter);
   var bpDao = BloodPressureDao(adapter);
-  var weightDao=WeightDao(adapter);
-  if(visit['temperatures']!=null){
-    for(Map tempMap in visit['temperatures']){
-      await tempDao.insertFromEhr(tempMap,personId,patientId);
+  var weightDao = WeightDao(adapter);
+  if (visit['temperatures'] != null) {
+    for (Map tempMap in visit['temperatures']) {
+      await tempDao.insertFromEhr(tempMap, personId, patientId);
     }
   }
 
-  if(visit['bloodPressures']!=null){
-    for(Map bpMap in visit['bloodPressures']){
-      await bpDao.insertFromEhr(bpMap,personId,patientId);
+  if (visit['bloodPressures'] != null) {
+    for (Map bpMap in visit['bloodPressures']) {
+      await bpDao.insertFromEhr(bpMap, personId, patientId);
     }
   }
 
-  if(visit['weight']!=null){
-      await weightDao.insertFromEhr(visit['weight'],personId,patientId);
+  if (visit['weight'] != null) {
+    await weightDao.insertFromEhr(visit['weight'], personId, patientId);
   }
-
-  return 'DONE_STATUS';
+  return '$DONE_STATUS';
 }
 
-Future<String> saveSexualHistory(Map sexualHistory) async{
+Future<String> saveSexualHistory(Map sexualHistory) async {
   var dbHandler = DatabaseHelper();
   var adapter = await dbHandler.getAdapter();
   var sexualHistoryDao = SexualHistoryDao(adapter);
   await sexualHistoryDao.insertFromEhr(sexualHistory);
-  return 'DONE_STATUS';
+  return '$DONE_STATUS';
 }
 
-Future <String> saveSexualHistoryQuestion(Map qns,String sexualHistoryId) async {
+Future<String> saveSexualHistoryQuestion(
+    Map qns, String sexualHistoryId) async {
   var dbHandler = DatabaseHelper();
   var adapter = await dbHandler.getAdapter();
   var shq = SexualHistoryQuestionDao(adapter);
-  for(Map qn in qns['questions']){
-    if(qn['sexualHistoryQuestionId']!=null){
-      shq.insertFromEhr(qn,sexualHistoryId);
+  for (Map qn in qns['questions']) {
+    if (qn['sexualHistoryQuestionId'] != null) {
+      shq.insertFromEhr(qn, sexualHistoryId);
     }
   }
   return '$DONE_STATUS';
 }
 
-Future <String> savePatientArt(Map map,String personId) async {
+Future<String> savePatientArt(Map map, String personId) async {
   var dbHandler = DatabaseHelper();
   var adapter = await dbHandler.getAdapter();
   var artDao = ArtDao(adapter);
-  artDao.insertFromEhr(map,personId);
+  await artDao.insertFromEhr(map, personId);
   return '$DONE_STATUS';
 }
 
-Future <String> savePatientArtSymptoms(Map map,String artId) async {
+Future<String> savePatientArtSymptoms(Map map, String artId) async {
   var dbHandler = DatabaseHelper();
   var adapter = await dbHandler.getAdapter();
   var artSymptomDao = ArtSymptomDao(adapter);
-  for(Map symptom in map['symptoms']){
+  for (Map symptom in map['symptoms']) {
     if (symptom['artSymptomId'] != null) {
-      await artSymptomDao.insertFromEhr(symptom,artId);
+      //await artSymptomDao.insertFromEhr(symptom, artId);
     }
+  }
+  return '$DONE_STATUS';
+}
 
+Future<String> saveArtAppointments(Map map) async {
+  var dbHandler = DatabaseHelper();
+  var adapter = await dbHandler.getAdapter();
+  var appointmentDao = ArtAppointmentDao(adapter);
+  for (Map appointment in map['appointments']) {
+    await appointmentDao.insertFromEhr(appointment);
   }
   return '$DONE_STATUS';
 }
