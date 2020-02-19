46cacee6691b9d4874b20f957bce8cc871a919fb
thomas <tomchanyowedza@gmail.com>
2020-02-19 08:00:45 +0200

    flut

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/thomas/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/thomas/Music/ehr_mobile"
+export "FLUTTER_APPLICATION_PATH=/home/thomas/Pictures/ehr_mobile"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
